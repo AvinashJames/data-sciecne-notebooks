76cec01948bd20e667b4c1e5b29be1c45ded8cd5
AvinashJames <avi17bondgmail.com>
2025-08-08 13:43:21 +0530

    Modified welcome message

--- first.py
+++ first.py
@@ -1,1 +1,2 @@
-print("I love my wife, whose name is Sweata Singh that too without 's' ")
+print("Welcome, This is the data scicene and machine learning python file")
+print("Here we will perform all the related task in this file")
